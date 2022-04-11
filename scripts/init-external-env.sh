4fc82aceac8baf8cf08c736ac4b7ce0d1135b50d
Sam McKelvie <dev@mckelvie.org>
2022-04-11 00:19:27 -0700

    init-external-env.sh

--- scripts/init-external-env.sh
+++ scripts/init-external-env.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-curl -sSL https://raw.githubusercontent.com/sammck/vpyapp/v0.2.0/vpyapp.py | python3 - -v run git+https://github.com/sammck/xpulumi.git xpulumi init-env
+set -eo pipefail
+
+echo "Please be patient..." >2
+curl https://raw.githubusercontent.com/sammck/vpyapp/v0.2.0/vpyapp.py | python3 - -v run git+https://github.com/sammck/xpulumi.git xpulumi init-env
