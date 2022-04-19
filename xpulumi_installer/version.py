00fe0aeac990ca88b218e260b6b96931f08240f0
semantic-release <semantic-release>
2022-04-19 14:59:33 -0700

    1.0.0
    Automatically generated by python-semantic-release

--- xpulumi_installer/version.py
+++ xpulumi_installer/version.py
@@ -10,6 +10,6 @@
 # __version = _metadata.version(__package__.replace('_','-')) #  e.g., '0.1.0'
 
 # The following line is automatically updated with "semantic-release version"
-__version__ =  "0.1.0"
+__version__ =  "1.0.0"
 
 __all__ = [ '__version__' ]
