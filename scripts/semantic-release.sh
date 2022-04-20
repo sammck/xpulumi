59db2f863d20c6719db8307bd2223d97451edf8f
Sam McKelvie <dev@mckelvie.org>
2022-04-19 20:26:01 -0700

    stable releease

--- scripts/semantic-release.sh
+++ scripts/semantic-release.sh
@@ -3,7 +3,16 @@
 set -eo pipefail
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PROJECT_DIR="$( dirname "$BIN_DIR" )"
+PROJECT_DIR="$( dirname "SCRIPT_DIR" )"
 
 cd "$PROJECT_DIR"
+
+git_status="$(git status --porcelain)"
+if [ -n "$git_status" ]; then
+  echo "ERROR: You have uncommitted git changes" >&2
+  exit 1
+fi
+git push
+
 GH_TOKEN="$(cat "$HOME/.private/github-semantic-versioning_token.txt")" semantic-release "$@"
+git push
